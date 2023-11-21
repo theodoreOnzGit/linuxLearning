cf15a7ca001a36f08603c0ba564f242dd6332f7e
ted at mint virt machine <theodore_ong@berkeley.edu>
2023-11-21 10:50:03 +0800

    changed the hop settings slightly

--- mint_setup/1_development_environment/nvim_files/hop_settings.lua
+++ mint_setup/1_development_environment/nvim_files/hop_settings.lua
@@ -2,6 +2,5 @@ local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 
 vim.keymap.set('', 'f', function()
-  hop.hint_words({ direction = directions.AFTER_CURSOR,
-  current_line_only = false })
+  hop.hint_words({ current_line_only = false })
 end, {remap=true})
