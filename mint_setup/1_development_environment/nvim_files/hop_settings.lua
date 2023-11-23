8656127acd71b4e608c49796d60f2e532cb21405
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2023-11-23 08:45:05 +0800

    configured hop settings

--- mint_setup/1_development_environment/nvim_files/hop_settings.lua
+++ mint_setup/1_development_environment/nvim_files/hop_settings.lua
@@ -1,7 +1,6 @@
 local hop = require('hop')
-local directions = require('hop.hint').HintDirection
+-- local directions = require('hop.hint').HintDirection
 
 vim.keymap.set('', 'f', function()
-  hop.hint_words({ direction = directions.AFTER_CURSOR,
-  current_line_only = false })
+  hop.hint_words({ current_line_only = false })
 end, {remap=true})
